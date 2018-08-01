f783c82375fd0c7e9ba3f07ad40cbba22b8b563e
HelloBetafish <li_knee@yahoo.com>
2018-08-01 16:32:20 -0700

    Heart ratings now changeable

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,6 +6,7 @@ if (!Array.isArray(list)) {
   list = [];
 }
 
+// Display list on webpage
 function display() {
   $("#list").empty(); // empties out the html
   var insideList = JSON.parse(localStorage.getItem("userlist"));
@@ -23,6 +24,9 @@ function display() {
       var b = $("<button class='delete'>").text("x").attr("data-index", i);
       // var heart = "<i class='material-icons heart' state='heart-empty'>favorite_border</i>";
       var heartsMult = $("<div class='hearts'>").attr("heart-index", insideList[i].hearts).attr("data-index", i);
+
+      heartsMult.append("<i class='material-icons undo'>undo</i>");
+
       var rating = insideList[i].hearts;
       for (var x = 0; x < rating; x++){
           var favHeart = "<i class='material-icons heart' heartNum=" + (x + 1) + 
@@ -43,24 +47,31 @@ function display() {
 // render our todos on page load
 display();
 
+// Add new entry
 $("#add").on("click", function(event) {
   event.preventDefault();
   // Setting the input value to a variable and then clearing the input
-  if($("#addChoice").val() !== ""){
-    var entryText = $("input[type='text']").val().trim();
-    $("input[type='text'").val("");
-    var heartVal = 5;
-    var entry = {
-      "Etext": entryText,
-      "hearts": heartVal
-    };
-    // Adding our new todo to our local list variable and adding it to local storage
-    list.push(entry);
-    localStorage.setItem("userlist", JSON.stringify(list));
-    display();
+  if (list.length < 10){
+    if($("#addChoice").val() !== ""){
+      var entryText = $("input[type='text']").val().trim();
+      $("input[type='text'").val("");
+      var heartVal = 0;
+      var entry = {
+        "Etext": entryText,
+        "hearts": heartVal
+      };
+      // Adding our new todo to our local list variable and adding it to local storage
+      list.push(entry);
+      localStorage.setItem("userlist", JSON.stringify(list));
+      display();
+      console.log(list.length)
+    }
+  }   else{
+    alert("Sorry, you only get 10 options. Please delete something.")
   }
 });
 
+// Clear entire list
 $("#clear").on("click", function(event) {
 event.preventDefault();
 var insidelist = JSON.parse(localStorage.getItem("userlist"));
@@ -74,6 +85,7 @@ var insidelist = JSON.parse(localStorage.getItem("userlist"));
   display();
 });
 
+// Delete entry
 $(document.body).on("click", "button.delete", function() {
   var insidelist = JSON.parse(localStorage.getItem("userlist"));
   var currentIndex = $(this).attr("data-index");
@@ -87,34 +99,38 @@ $(document.body).on("click", "button.delete", function() {
   display();
 });
 
+// Change heart rating
 $(document.body).on("click", ".heart", function(){
-  var hNum = $(this).attr("heartNum");
-  var dNum = $(this).parent().attr("data-index");
-  console.log(hNum);
-  console.log(dNum);
+    var hNum = parseInt($(this).attr("heartNum"));
+    var dNum = parseInt($(this).parent().attr("data-index"));
+    // console.log(hNum);
+    // console.log(dNum);
+
+    var insidelist = JSON.parse(localStorage.getItem("userlist"));
+    insidelist[dNum].hearts=hNum;
+    // console.log("New heart num: " + insidelist[dNum].hearts);
+
+    list = insidelist;
+
+    localStorage.setItem("userlist", JSON.stringify(insidelist));
+
+    display();
 });
 
-// Function to change gif state between animated and still when clicked
-// $(document.body).on("click", ".heart", function(){
-//   var hState = $(this).attr("state");
-//   var nextState = $(this).next().attr("state");
-//   if (hState === "heart-empty") {
-//     $(this).text("favorite");
-//     $(this).prevAll().text("favorite");
-//     $(this).attr("state", "heart-full");
-//     $(this).prevAll().attr("state", "heart-full");
-//     $(this).nextAll().text("favorite_border");
-//     $(this).nextAll().attr("state", "heart-empty");
-//   }
-//   else if (hState === "heart-full" && $(this).is(':first-child') && nextState ==="heart-empty") {
-//   	$(this).text("favorite_border");
-//   	$(this).attr("state", "heart-empty")
-//   }
-//   else {
-//     $(this).nextAll().text("favorite_border");
-//     $(this).nextAll().attr("state", "heart-empty");
-//   }
-// });
+// Zero out hearts
+$(document.body).on("click", ".undo", function(){
+    var dNum = parseInt($(this).parent().attr("data-index"));
+
+    var insidelist = JSON.parse(localStorage.getItem("userlist"));
+    insidelist[dNum].hearts=0;
+
+    list = insidelist;
+
+    localStorage.setItem("userlist", JSON.stringify(insidelist));
+
+    display();
+});
+
 
 var askJessica = function (){
   var insidelist = JSON.parse(localStorage.getItem("userlist"));
